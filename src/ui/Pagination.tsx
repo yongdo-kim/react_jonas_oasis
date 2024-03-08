90eac51e77191e1c0ebc6727022aa7585996f7a4
yongdo-kim <matthewkim@Matthewui-MacBookPro.local>
2024-03-08 18:46:21 +0900

    feat nexgt previous button

--- src/ui/Pagination.tsx
+++ src/ui/Pagination.tsx
@@ -1,11 +1,27 @@
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import styles from "./Pagination.module.css";
 
 export default function Pagination() {
+  function nextPage() {}
+
+  function previousPage() {}
+
   return (
     <div className={styles.pagination}>
       <p className={styles.active}>
         Showing <span>1</span> to <span>10</span> of <span>23</span> results
       </p>
+
+      <div className={styles.buttons}>
+        <button className={styles.paginationButton}>
+          <HiChevronLeft />
+          <span>Previous</span>
+        </button>
+        <button className={styles.paginationButton}>
+          <span>Next</span>
+          <HiChevronRight />
+        </button>
+      </div>
     </div>
   );
 }
