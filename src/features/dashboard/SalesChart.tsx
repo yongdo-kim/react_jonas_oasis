02f7c96fe39aa4e6f2b8557464da1bb5f817b7ab
yongdo-kim <matthewkim@Matthewui-MacBookPro.local>
2024-03-14 15:18:41 +0900

    remove 사용되지 않는 코드 제거

--- src/features/dashboard/SalesChart.tsx
+++ src/features/dashboard/SalesChart.tsx
@@ -10,41 +10,8 @@ import {
 } from "recharts";
 import { useDarkMode } from "../../context/DarkModeContext";
 import Heading from "../../ui/Heading";
-import { BookingProps } from "../bookings/BookingRow";
 import styles from "./SalesChart.module.css";
 import { RecentBookingsProps } from "./useRecentBookings";
-
-const fakeData = [
-  { label: "Jan 09", totalSales: 480, extrasSales: 20 },
-  { label: "Jan 10", totalSales: 580, extrasSales: 100 },
-  { label: "Jan 11", totalSales: 550, extrasSales: 150 },
-  { label: "Jan 12", totalSales: 600, extrasSales: 50 },
-  { label: "Jan 13", totalSales: 700, extrasSales: 150 },
-  { label: "Jan 14", totalSales: 800, extrasSales: 150 },
-  { label: "Jan 15", totalSales: 700, extrasSales: 200 },
-  { label: "Jan 16", totalSales: 650, extrasSales: 200 },
-  { label: "Jan 17", totalSales: 600, extrasSales: 300 },
-  { label: "Jan 18", totalSales: 550, extrasSales: 100 },
-  { label: "Jan 19", totalSales: 700, extrasSales: 100 },
-  { label: "Jan 20", totalSales: 800, extrasSales: 200 },
-  { label: "Jan 21", totalSales: 700, extrasSales: 100 },
-  { label: "Jan 22", totalSales: 810, extrasSales: 50 },
-  { label: "Jan 23", totalSales: 950, extrasSales: 250 },
-  { label: "Jan 24", totalSales: 970, extrasSales: 100 },
-  { label: "Jan 25", totalSales: 900, extrasSales: 200 },
-  { label: "Jan 26", totalSales: 950, extrasSales: 300 },
-  { label: "Jan 27", totalSales: 850, extrasSales: 200 },
-  { label: "Jan 28", totalSales: 900, extrasSales: 100 },
-  { label: "Jan 29", totalSales: 800, extrasSales: 300 },
-  { label: "Jan 30", totalSales: 950, extrasSales: 200 },
-  { label: "Jan 31", totalSales: 1100, extrasSales: 300 },
-  { label: "Feb 01", totalSales: 1200, extrasSales: 400 },
-  { label: "Feb 02", totalSales: 1250, extrasSales: 300 },
-  { label: "Feb 03", totalSales: 1400, extrasSales: 450 },
-  { label: "Feb 04", totalSales: 1500, extrasSales: 500 },
-  { label: "Feb 05", totalSales: 1400, extrasSales: 600 },
-  { label: "Feb 06", totalSales: 1450, extrasSales: 400 },
-];
 
 export default function SalesChart({
   bookings,
@@ -86,26 +53,26 @@ export default function SalesChart({
 
   return (
     <div className={styles.styledSalesChart}>
-      <Heading style='h2'>Sales</Heading>
-      <ResponsiveContainer height={300} width='100%'>
+      <Heading style="h2">Sales</Heading>
+      <ResponsiveContainer height={300} width="100%">
         <AreaChart data={data}>
-          <CartesianGrid strokeDasharray='4' />
+          <CartesianGrid strokeDasharray="4" />
           <XAxis
-            dataKey='label'
+            dataKey="label"
             tick={{ fill: colors.text }}
             tickLine={{ stroke: colors.text }}
           />
-          <YAxis unit='$' />
+          <YAxis unit="$" />
           <Tooltip />
           <Area
-            dataKey='totalSales'
-            type='monotone'
+            dataKey="totalSales"
+            type="monotone"
             stroke={colors.totalSales.stroke}
             fill={colors.totalSales.fill}
           />
           <Area
-            dataKey='extrasSales'
-            type='monotone'
+            dataKey="extrasSales"
+            type="monotone"
             stroke={colors.extrasSales.stroke}
             fill={colors.extrasSales.fill}
           />
